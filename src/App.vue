9678bf73346001ccf46da624e293ac1de7eafec7
Sofia Ling <sofialing@live.se>
2020-01-07 17:19:33 +0100

    Added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
+
+
+<script>
+import Navbar from "@/layout/Navbar.vue";
+
+export default {
+  components: {
+    Navbar
+  }
+};
+</script>
+
 
 <style lang="scss">
 </style>
---
+++ src/layout/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img :src="logo" alt="Cat shop" width="150px" />
+      </a>
+
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <router-link to="/" class="navbar-item">Start</router-link>
+        <a class="navbar-item">Produkter</a>
+        <router-link to="/about" class="navbar-item">Om oss</router-link>
+        <a class="navbar-item">Kontakt</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      logo: require("@/assets/logo.svg")
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.navbar-item {
+  text-transform: uppercase;
+}
+</style>
