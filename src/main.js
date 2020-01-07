07688a152db0ca9b34b7c6cce7d21b6330b249f3
Sofia Ling <sofialing@live.se>
2020-01-07 17:15:45 +0100

    Installed and implemented bulma and cleaned up unnecessary code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+require('./assets/main.scss');
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	render: h => h(App)
+}).$mount('#app');
