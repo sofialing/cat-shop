cedef8addc29074e9d7665a22996eed2a2bbca4e
Sofia Ling <sofialing@live.se>
2020-01-08 16:07:02 +0100

    Added hero section for subpages

---
+++ src/components/HeroSubpage.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="hero is-primary is-medium">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns is-centered">
+          <div class="column is-two-thirds has-text-centered">
+            <h1 class="title is-1 has-text-weight-bold">{{ title }}</h1>
+            <p v-show="subtitle" class="content is-medium">{{ subtitle }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HeroSubpage",
+  props: {
+    title: {
+      type: String
+    },
+    subtitle: {
+      type: String
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hero {
+  background-image: url("../assets/images/hero-sub.jpg");
+  background-size: cover;
+  background-position: center;
+}
+</style>
