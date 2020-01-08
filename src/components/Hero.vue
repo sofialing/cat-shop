74accc52229cfb8846888035fcf2332f5428b6f6
Sofia Ling <sofialing@live.se>
2020-01-08 13:23:48 +0100

    Updated hero and intro section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,8 +2,16 @@
   <section class="hero is-primary is-large">
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">Large title</h1>
-        <h2 class="subtitle">Large subtitle</h2>
+        <div class="columns">
+          <div class="column is-half">
+            <h1 class="title is-1 has-text-weight-bold">Vi gör din katt lycklig!</h1>
+            <h2 class="subtitle is-5">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Suscipit voluptatem aliquid ipsam distinctio ipsum
+              eveniet earum a voluptas culpa quia.
+            </h2>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -17,7 +25,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-section {
-  margin-bottom: 3rem;
+.hero {
+  background-image: url("../assets/images/hero-test.jpg");
+  background-size: cover;
+  background-position: center;
+
+  .title {
+    font-size: 4rem;
+  }
 }
 </style>
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,25 +1,31 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <div class="columns is-centered">
-        <div class="column is-three-fifths has-text-centered">
-          <h1 class="title is-2">Din kattbutik online med intressanta kattillbehör och kattsaker</h1>
-          <p
-            class="content"
-          >Vårt sortiment utökas ständigt, vilket innebär att du alltid hittar ny spännande kattutrustning till din katt hos oss. Katter älskar att leka, så det är viktigt att din katt har möjlighet för att hålla sig aktiv - speciellt när den är ensam hemma. Genom att investera i rätt kattutrustning skonar du även dina möbler och krukväxter, m.m.</p>
-          <a href="#" class="button is-primary">Läs mer</a>
-        </div>
-      </div>
-    </div>
-  </section>
+	<section class="section">
+		<div class="container">
+			<div class="columns is-centered">
+				<div class="column is-two-thirds has-text-centered">
+					<h1 class="title is-1 has-text-weight-bold">
+						Din kattbutik online med intressanta kattillbehör och
+						kattsaker
+					</h1>
+					<p class="content is-medium">
+						Vårt sortiment utökas ständigt, vilket innebär att du alltid
+						hittar ny spännande kattutrustning till din katt hos oss.
+						Katter älskar att leka, så det är viktigt att din katt har
+						möjlighet för att hålla sig aktiv - speciellt när den är
+						ensam hemma. Genom att investera i rätt kattutrustning skonar
+						du även dina möbler och krukväxter, m.m.
+					</p>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
 export default {
-  name: "Intro"
+	name: 'Intro'
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-</style>
+<style scoped lang="scss"></style>
