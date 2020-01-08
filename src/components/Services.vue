4de59c1a2baefd32bf1da51cb56ceeb8725067ed
Sofia Ling <sofialing@live.se>
2020-01-08 11:29:01 +0100

    Added services component

---
+++ src/components/Services.vue
@@ -0,0 +1,64 @@
+<template>
+	<section class="section has-text-centered">
+		<div class="container">
+			<div class="columns">
+				<div class="column is-one-third">
+					<div class="box has-background-white-ter">
+						<truck-icon class="icon-2x" />
+						<p class="is-uppercase">Snabba leveranser</p>
+						<p class="is-size-7">
+							Vi skickar dina varor inom en arbetsdag
+						</p>
+					</div>
+				</div>
+				<div class="column is-one-third">
+					<div class="box has-background-white-ter">
+						<shopping-icon class="icon-2x" />
+						<p class="is-uppercase">Fri frakt</p>
+						<p class="is-size-7">Alltid fri leverans på alla ordrar</p>
+					</div>
+				</div>
+				<div class="column is-one-third">
+					<div class="box has-background-white-ter">
+						<phone-icon class="icon-2x" />
+						<p class="is-uppercase">Professionell service</p>
+						<p class="is-size-7">Online support dygnet runt</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+import TruckIcon from 'vue-material-design-icons/TruckFastOutline.vue';
+import ShoppingIcon from 'vue-material-design-icons/ShoppingOutline.vue';
+import PhoneIcon from 'vue-material-design-icons/PhoneInTalkOutline.vue';
+
+export default {
+	name: 'Services',
+	components: {
+		TruckIcon,
+		ShoppingIcon,
+		PhoneIcon
+	}
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.box {
+	border-radius: 0;
+	box-shadow: none;
+}
+
+.material-design-icon.icon-2x {
+	height: 1.5em;
+	width: 1.5em;
+}
+
+.material-design-icon.icon-2x > .material-design-icon__svg {
+	height: 2em;
+	width: 2em;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="home">
-    <Hero />
-    <Intro />
-    <FeatureProducts :products="products.bestSellers" title="Mest populära" />
-    <FeatureSection />
-    <FeatureProducts :products="products.bestSellers" title="Nytt i butiken" />
-  </div>
+	<div class="home">
+		<Hero />
+		<Intro />
+		<Services />
+		<FeatureProducts :products="products.bestSellers" title="Mest populära" />
+		<FeatureSection />
+		<FeatureProducts :products="products.bestSellers" title="Nytt i butiken" />
+	</div>
 </template>
 
 <script>
-import Hero from "@/components/Hero.vue";
-import Intro from "@/components/Intro.vue";
-import FeatureProducts from "@/components/FeatureProducts.vue";
-import FeatureSection from "@/components/FeatureSection.vue";
+import Hero from '@/components/Hero.vue';
+import Intro from '@/components/Intro.vue';
+import Services from '@/components/Services.vue';
+import FeatureProducts from '@/components/FeatureProducts.vue';
+import FeatureSection from '@/components/FeatureSection.vue';
 
-import { Products } from "@/services/Products";
+import { Products } from '@/services/Products';
 
 export default {
-  name: "home",
-  components: {
-    Hero,
-    Intro,
-    FeatureProducts,
-    FeatureSection
-  },
-  data() {
-    return {
-      products: new Products()
-    };
-  }
+	name: 'home',
+	components: {
+		Hero,
+		Intro,
+		Services,
+		FeatureProducts,
+		FeatureSection
+	},
+	data() {
+		return {
+			products: new Products()
+		};
+	}
 };
 </script>
