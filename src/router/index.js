88aeaae2f34884672bfd698542c201957ab46838
Sofia Ling <sofialing@live.se>
2020-01-09 12:56:52 +0100

    Fixed issue with router settings

--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,27 @@ const routes = [
 			import(/* webpackChunkName: "about" */ '../views/Products.vue')
 	},
 	{
+		path: '/products/:slug',
+		name: 'products',
+		props: true,
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ '../views/Products.vue')
+	},
+	// {
+	// 	path: '/products/view/:post-slug',
+	// 	name: 'product',
+	// 	// route level code-splitting
+	// 	// this generates a separate chunk (about.[hash].js) for this route
+	// 	// which is lazy-loaded when the route is visited.
+	// 	component: () =>
+	// 		import(
+	// 			/* webpackChunkName: "about" */ '../components/ProductDetails.vue'
+	// 		)
+	// },
+	{
 		path: '/contact',
 		name: 'contact',
 		// route level code-splitting
@@ -39,6 +60,8 @@ const routes = [
 ];
 
 const router = new VueRouter({
+	mode: 'history',
+	base: __dirname,
 	routes
 });
 
